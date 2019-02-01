29e9c73825a3672a4e0d65fc721db049371ec9f0
poychang <poypost@gmail.com>
2019-02-01 12:38:49 +0800

    init project

---
+++ ThreeStepWebLoginApp/Models/AppSettingsModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreeStepWebLoginApp.Models
+{
+    public class AppSettingsModel
+    {
+        public string Url { get; set; }
+        public int DelayClose { get; set; }
+        public bool HideWindow { get; set; }
+        public LoginInfo LoginInfo { get; set; }
+        public StepXpath StepXpath { get; set; }
+    }
+
+    public class LoginInfo
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class StepXpath
+    {
+        public string Step1 { get; set; }
+        public string Step2 { get; set; }
+        public string Step3 { get; set; }
+    }
+}
--- ThreeStepWebLoginApp/Program.cs
+++ ThreeStepWebLoginApp/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Threading;
+using ThreeStepWebLoginApp.Models;
 
 namespace ThreeStepWebLoginApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Three Step Web Login App");
+            Console.WriteLine("==============================");
+            Run(ReadFromAppSettings().Get<AppSettingsModel>());
+        }
+
+        public static IConfigurationRoot ReadFromAppSettings()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+
+        public static void Run(AppSettingsModel config)
+        {
+            var chromeOptions = new ChromeOptions();
+            var arguments = config.HideWindow
+                ? /*隱藏 Chrome 視窗*/    "headless"
+                : /*設定 Chrome 視窗大小*/ "--window-size=1024,800";
+            chromeOptions.AddArgument(arguments);
+
+            using (var driver = new ChromeDriver("./", chromeOptions))
+            {
+                // 開啟網頁
+                driver.Url = config.Url;
+                // 動作
+                driver.FindElement(By.XPath(config.StepXpath.Step1)).SendKeys(config.LoginInfo.Account);
+                driver.FindElement(By.XPath(config.StepXpath.Step2)).SendKeys(config.LoginInfo.Password);
+                driver.FindElement(By.XPath(config.StepXpath.Step3)).Click();
+                Thread.Sleep(TimeSpan.FromSeconds(config.DelayClose));
+            }
         }
     }
 }
